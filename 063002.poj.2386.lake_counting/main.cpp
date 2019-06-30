e176f39739d1ed30938e7723151c6cf54cec4e6d
hanlinghao <417564526@qq.com>
2019-07-01 00:05:47 +0800

    update 0630

--- 063002.poj.2386.lake_counting/main.cpp
+++ 063002.poj.2386.lake_counting/main.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 using namespace std;
 int n, m;
-int field[100][100] = { 0 };
+int field[100][100] = {0};
 int offset_x[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
 int offset_y[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
 
 bool mark(int x, int y, int count) {
-    if (x >= n || x <0 || y < 0 || y >= m || field[x][y] != 0) {
+    if (x >= n || x < 0 || y < 0 || y >= m || field[x][y] != 0) {
         return false;
     }
     field[x][y] = count;
-    for(int i = 0; i < 8; i++) {
-        mark(x+offset_x[i], y+offset_y[i], count);
+    for (int i = 0; i < 8; i++) {
+        mark(x + offset_x[i], y + offset_y[i], count);
     }
     return true;
 }
@@ -32,7 +32,7 @@ int main() {
     }
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            if(mark(i, j, number+1)){
+            if (mark(i, j, number + 1)) {
                 number++;
             }
         }
--- 063004.bailian.3752.走迷宫/main.cpp
+++ 063004.bailian.3752.走迷宫/main.cpp
@@ -6,7 +6,6 @@ using namespace std;
 
 typedef pair<int, int> position;
 queue<position *> pos_queue;
-position last[40][40];
 int maze[40][40];
 int dx[4] = {-1, 0, 1, 0};
 int dy[4] = {0, -1, 0, 1};
@@ -24,7 +23,6 @@ void bfs(int i, int j) {
             newy = front->second + dy[i];
             if (newx >= 0 && newx <= n && newy >= 0 && newy <= m && maze[newx][newy] == 0) {
                 next = new position(newx, newy);
-                last[newx][newy] = *front;
                 maze[newx][newy] = maze[front->first][front->second] + 1;
                 pos_queue.push(next);
                 if (newx == n - 1 && newy == m - 1) {
@@ -37,24 +35,18 @@ void bfs(int i, int j) {
 }
 
 int main() {
-    n = m = 5;
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 5; j++) {
-            cin >> maze[i][j];
+    cin >> n >> m;
+    char c;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> c;
+            if (c == '.') {
+                maze[i][j] = 0;
+            } else {
+                maze[i][j] = -1;
+            }
         }
     }
     bfs(0, 0);
-    position p(n - 1, n - 1), zero(0, 0);
-    stack<position> pos_stack;
-    while (p != zero) {
-        pos_stack.push(last[p.first][p.second]);
-        p = last[p.first][p.second];
-    }
-    while (!pos_stack.empty()) {
-        p = pos_stack.top();
-        printf("(%d, %d)\n", p.first, p.second);
-        pos_stack.pop();
-    }
-    printf("(%d, %d)\n", n - 1, m - 1);
-    // printf("%d", maze[n - 1][m - 1]);
+    printf("%d", maze[n - 1][m - 1]);
 }
